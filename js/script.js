cb254593bf6aa28d426f758a933e20f29c8ba6c1
Lynn Mitchell <lynn7mitchell@gmail.com>
2022-05-26 18:13:27 -0500

    add biscuit noises

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 const rubberBuiscuitFullSong = new Audio("../music/rubber-biscuit.mp3");
+const first = new Audio("../music/first.wav");
+const second = new Audio("../music/second.wav");
+const sounds = [first, second];
+
+let counter = 0;
+
 const onBiscuitClick = () => {
-  rubberBuiscuitFullSong.play();
+  sounds[counter].play();
+  if (counter === sounds.length - 1) {
+    counter = 0;
+  } else {
+    counter += 1;
+  }
 };
